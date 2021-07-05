f7cd206819cd3706e13ab07aeb027efdb6182914
Aaron <71854919+aaroncodehub@users.noreply.github.com>
2021-07-06 10:18:40 +1200

    Update fb.js
    update config

--- fb.js
+++ fb.js
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 
 // firebase init goes here
 const config = {
-    apiKey: "AIzaSyAll-VaHFJWg6jwQ-BPnx80g6QAdNrMb1s",
-    authDomain: "mysharpeyemate.firebaseapp.com",
-    databaseURL: "https://mysharpeyemate.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "mysharpeyemate",
-    storageBucket: "mysharpeyemate.appspot.com",
-    messagingSenderId: "67037945546",
-    appId: "1:67037945546:web:1df278cc49c23a3bef9e33",
-    measurementId: "G-274K5GPPX7"
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 firebase.initializeApp(config)
 
